8d031cc011523f58d184fa11bb40e1606868d93d
bwty <whalelephant@users.noreply.github.com>
2022-04-02 23:45:42 -0400

    feat(wip): gov-token

---
+++ contracts/gov-tokens/src/error.rs
@@ -0,0 +1,35 @@
+use cosmwasm_std::{StdError, Uint128};
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+    #[error("Cannot set to own account")]
+    CannotSetOwnAccount {},
+
+    #[error("Invalid zero amount")]
+    InvalidZeroAmount {},
+
+    #[error("Minting cannot exceed the cap")]
+    CannotExceedCap {},
+
+    #[error("Logo binary data exceeds 5KB limit")]
+    LogoTooBig {},
+
+    #[error("Invalid xml preamble for SVG")]
+    InvalidXmlPreamble {},
+
+    #[error("Invalid png header")]
+    InvalidPngHeader {},
+
+    #[error("Incorrect voting power for burning: {0}")]
+    IncorrectBalance(Uint128),
+
+    #[error("Wallet not found")]
+    NotFound {},
+}
